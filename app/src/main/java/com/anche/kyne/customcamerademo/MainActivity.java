ec0c49331c9257be42e8553ea35c451ca70de555
1020130342@qq.com <1020130342@qq.com>
2017-11-04 17:19:54 +0800

    增加屏幕适配

--- app/src/main/java/com/anche/kyne/customcamerademo/MainActivity.java
+++ app/src/main/java/com/anche/kyne/customcamerademo/MainActivity.java
@@ -1,22 +1,28 @@
 package com.anche.kyne.customcamerademo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
+    private static final String TAG = "MainActivity";
     private SurfaceView mSurfaceView;
     private Camera mCamera;
     private SurfaceHolder mHolder;
@@ -58,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                 mCamera.autoFocus(null);
             }
         });
+        Log.i(TAG, "onCreate: ");
     }
 
     /**
@@ -66,9 +73,8 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
      * @param view
      */
     public void capture(View view) {
-        Camera.Parameters parameters = mCamera.getParameters();
-        parameters.setPictureFormat(ImageFormat.JPEG);
-        parameters.setFocusMode(Camera.Parameters.ANTIBANDING_AUTO);
+        float screenRatio = getScreenRatio(this);
+        setCameraParams(mCamera, screenRatio);
         mCamera.autoFocus(new Camera.AutoFocusCallback() {
             @Override
             public void onAutoFocus(boolean b, Camera camera) {
@@ -134,6 +140,86 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
             mCamera.release();
             mCamera = null;
         }
+    }
+
+    private void setCameraParams(Camera camera, float screenRatio) {
+        Camera.Parameters parameters = camera.getParameters();
+        // 获取摄像头支持的PictureSize列表
+        List<Camera.Size> pictureSizeList = parameters.getSupportedPictureSizes();
+        for (Camera.Size size : pictureSizeList) {
+            Log.i(TAG, "pictureSizeList size.width=" + size.width + "  size.height=" + size.height);
+        }
+        /**从列表中选取合适的分辨率*/
+        Camera.Size picSize = getProperSize(pictureSizeList, screenRatio);
+        //        超级高清版，压缩后大约维持在200KB左右
+        //        Camera.Size picSize =pictureSizeList.get(10);
+        //        基础清晰版,100KB以下
+        //        Camera.Size picSize = pictureSizeList.get(5);
+        if (null == picSize) {
+            Log.i(TAG, "null == picSize");
+            picSize = parameters.getPictureSize();
+        }
+        Log.i(TAG, "picSize.width=" + picSize.width + "  picSize.height=" + picSize.height);
+        // 根据选出的PictureSize重新设置SurfaceView大小
+        parameters.setPictureSize(picSize.width, picSize.height);
+        // 获取摄像头支持的PreviewSize列表
+        List<Camera.Size> previewSizeList = parameters.getSupportedPreviewSizes();
+        for (Camera.Size size : previewSizeList) {
+            Log.i(TAG, "previewSizeList size.width=" + size.width + "  size.height=" + size.height);
+        }
+        Camera.Size preSize = getProperSize(previewSizeList, screenRatio);
+        if (null != preSize) {
+            Log.i(TAG, "preSize.width=" + preSize.width + "  preSize.height=" + preSize.height);
+            parameters.setPreviewSize(preSize.width, preSize.height);
+        }
+        parameters.setJpegQuality(100); // 设置照片质量
+        if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);// 连续对焦模式
+        }
+        //设置相机闪光灯，三种形式
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        parameters.setPictureFormat(ImageFormat.JPEG);
+        parameters.setFocusMode(Camera.Parameters.ANTIBANDING_AUTO);
+
+    }
+
+    /**
+     * 从列表中选取合适的分辨率
+     * 默认w:h = 4:3
+     * <p>注意：这里的w对应屏幕的height
+     * h对应屏幕的width<p/>
+     */
+    private Camera.Size getProperSize(List<Camera.Size> pictureSizeList, float screenRatio) {
+        Log.i(TAG, "screenRatio=" + screenRatio);
+        Camera.Size result = null;
+        for (Camera.Size size : pictureSizeList) {
+            float currentRatio = ((float) size.width) / size.height;
+            if (currentRatio - screenRatio == 0) {
+                result = size;
+                break;
+            }
+        }
+
+        if (null == result) {
+            for (Camera.Size size : pictureSizeList) {
+                float curRatio = ((float) size.width) / size.height;
+                if (curRatio == 4f / 3) {// 默认w:h = 4:3
+                    result = size;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private float getScreenRatio(Context context) {
+        WindowManager WM = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        WM.getDefaultDisplay().getMetrics(outMetrics);
+        int screenWidth = outMetrics.heightPixels;
+        int screenHeight = outMetrics.widthPixels;
+        return (float) screenHeight / screenWidth;
     }
 
     @Override
